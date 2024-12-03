72f956ae46dd0a24c4b301e0e9dedbc803e47ce9
Dave Ziegelbauer <dziegelbauer@gmail.com>
2024-12-02 21:36:26 -0800

    Day 3 Complete

--- AOC2024/Day03.cs
+++ AOC2024/Day03.cs
@@ -1,19 +1,85 @@
+using System.Text.RegularExpressions;
+
 namespace AOC2024;
 
-public class Day03
+public partial class Day03
 {
+    private readonly string _input;
     public Day03(string inputFilePath)
     {
-        
+        _input = File.ReadAllText(inputFilePath);
     }
 
     public long Execute()
     {
-        return 0;
+        var instructionRegex = MultiplyRegex();
+        
+        var matches = instructionRegex.Matches(_input);
+        
+        long finalResult = 0;
+
+        foreach (Match match in matches)
+        {
+            var firstNumber = long.Parse(match.Groups[1].Value);
+            var secondNumber = long.Parse(match.Groups[2].Value);
+            var result = firstNumber * secondNumber;
+            finalResult += result;
+        }
+        
+        return finalResult;
     }
 
     public long ExecutePart2()
     {
-        return 0;
+        var multiplyRegex = MultiplyRegex();
+        var doRegex = DoRegex();
+        var dontRegex = DontRegex();
+        
+        var nextIndex = 0;
+        var runningTotal = 0L;
+
+        while (nextIndex < _input.Length)
+        {
+            var nextMultiply = multiplyRegex.Match(_input, nextIndex);
+            var nextDo = doRegex.Match(_input, nextIndex);
+            var nextDont = dontRegex.Match(_input, nextIndex);
+
+            if (nextMultiply.Success && (!nextDo.Success || nextDont.Index > nextMultiply.Index) &&
+                (!nextDont.Success || nextDont.Index > nextMultiply.Index))
+            {
+                nextIndex = nextMultiply.Index + nextMultiply.Length;
+                runningTotal += long.Parse(nextMultiply.Groups[1].Value) * long.Parse(nextMultiply.Groups[2].Value);
+            }
+            else if (nextDont.Success && (!nextMultiply.Success || nextDont.Index < nextMultiply.Index) && (!nextDo.Success || nextDont.Index < nextDo.Index))
+            {
+                if (nextDo.Success)
+                {
+                    nextIndex = nextDo.Index + nextDo.Length;
+                }
+                else
+                {
+                    nextIndex = _input.Length;
+                }
+            }
+            else if(nextDo.Success)
+            {
+                nextIndex = nextDo.Index + nextDo.Length;
+            }
+            else
+            {
+                nextIndex = _input.Length;
+            }
+        }
+        
+        return runningTotal;
     }
+
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    private static partial Regex MultiplyRegex();
+
+    [GeneratedRegex(@"do\(\)")]
+    private static partial Regex DoRegex();
+
+    [GeneratedRegex(@"don\'t\(\)")]
+    private static partial Regex DontRegex();
 }
--- AdventofCodeTests/Day03Tests.cs
+++ AdventofCodeTests/Day03Tests.cs
@@ -5,28 +5,26 @@ namespace AdventOfCodeTests;
 public class Day03Tests
 {
     private string _inputFilePath = "";
-    
-    [SetUp]
-    public void Setup()
-    {
-        _inputFilePath = "/Users/dave/Projects/AdventOfCode2024/Input/test03.txt";
-    }
 
     [Test]
     public void Day03Part01ProducesExampleOutput()
     {
+        _inputFilePath = "/Users/dave/Projects/AdventOfCode2024/Input/test03.txt";
+        
         var day3Part1 = new Day03(_inputFilePath);
         var result = day3Part1.Execute();
 
-        Assert.That(result, Is.EqualTo(2));
+        Assert.That(result, Is.EqualTo(161));
     }
     
     [Test]
     public void Day03Part02ProducesExampleOutput()
     {
+        _inputFilePath = "/Users/dave/Projects/AdventOfCode2024/Input/test03-2.txt";
+        
         var day3Part2 = new Day03(_inputFilePath);
         var result = day3Part2.ExecutePart2();
         
-        Assert.That(result, Is.EqualTo(4));
+        Assert.That(result, Is.EqualTo(48));
     }
 }
