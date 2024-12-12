8c476e983e8ea808ce44254012fe26ca19cda657
Dave Ziegelbauer <dziegelbauer@gmail.com>
2024-12-11 20:14:28 -0800

    Day 11 complete

---
+++ AdventofCodeTests/Day11Tests.cs
@@ -0,0 +1,17 @@
+using AOC2024;
+
+namespace AdventOfCodeTests;
+
+public class Day11Tests
+{
+    private readonly string _inputFilePath = "/Users/dave/Projects/AdventOfCode2024/Input/test11.txt";
+
+    [Test]
+    public async Task Day11Part01ProducesExampleOutput()
+    {
+        var day11Part1 = new Day11(_inputFilePath);
+        var result = await day11Part1.Execute();
+
+        Assert.That(result, Is.EqualTo(55312));
+    }
+}
